2f37fd8f17b2d2a194a1d6e0c7c3d15e2ffeee34
문태준 <pjrnoon99@gmail.com>
2024-05-19 22:16:44 +0900

    [feature] MemberService

--- src/main/java/tripvibe/tripvibebe/service/MemberService.java
+++ src/main/java/tripvibe/tripvibebe/service/MemberService.java
@@ -1,4 +1,58 @@
 package tripvibe.tripvibebe.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tripvibe.tripvibebe.domain.Member;
+import tripvibe.tripvibebe.repository.MemberRepository;
+import tripvibe.tripvibebe.repository.MemberRepositoryImpl;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepositoryImpl memberRepositoryImpl;
+
+    /**
+     * 회원가입
+     */
+    @Transactional(readOnly = false)
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepositoryImpl.save(member);
+
+        return member.getId();
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepositoryImpl.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepositoryImpl.findOne(memberId);
+    }
+
+
+
+
+    // 중복 회원 검증 메서드
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepositoryImpl.findByName(member.getUsername());// 같은 이름이 있는지 찾아봄
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 있는 아이디임");
+        }
+    }
+
+
 }
+
+
