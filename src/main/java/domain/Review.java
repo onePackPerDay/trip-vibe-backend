bb09e7353dc5f7d438f6d6a707e0d52ce05b221a
heo5620 <tnqls1035@naver.com>
2024-05-19 16:09:31 +0900

    [feat] User Entity, Review Entity 생성

---
+++ src/main/java/domain/Review.java
@@ -0,0 +1,35 @@
+package domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Review {
+
+    @Id //primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본 키 값을 자동으로 생성
+    private  Long id;
+
+    @Column(nullable = false)
+    private  String title;
+
+    @Lob
+    private byte[] img;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private int rating;
+
+    private LocalDate createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
