1ce1983bc2c310459c7dfaecef9981b89cec83cb
문태준 <pjrnoon99@gmail.com>
2024-05-19 14:39:16 +0900

    commit

---
+++ src/main/java/tripvibe/tripvibebe/TripVibeBeApplication.java
@@ -0,0 +1,13 @@
+package tripvibe.tripvibebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripVibeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripVibeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tripvibe/tripvibebe/TripVibeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tripvibe.tripvibebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripVibeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
