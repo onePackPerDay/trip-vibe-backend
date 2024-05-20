39e6ac50b61b4b99072de2912ce1d2e07f8fd103
문태준 <pjrnoon99@gmail.com>
2024-05-20 16:47:35 +0900

    [test] 중복 가입 테스트

--- src/test/java/tripvibe/tripvibebe/service/MemberServiceTest.java
+++ src/test/java/tripvibe/tripvibebe/service/MemberServiceTest.java
@@ -30,10 +30,10 @@ class MemberServiceTest {
     // 정상
     @Test
     @DisplayName("회원가입 테스트")
-    @Rollback(value = false)
-    public void saveMemberTest() {
+    @Rollback
+    public void 회원가입() {
         // given
-        Member member = createMember();
+        Member member = createMember("문");
 
         // when
         Long join = memberService.join(member);
@@ -44,12 +44,31 @@ class MemberServiceTest {
         assertEquals(member.getUsername(), savedMember.getUsername());
     }
 
+    @Test
+    @DisplayName("중복 가입 테스트")
+    @Rollback
+    public void 중복_회원가입_예외() {
+        // given
+        Member member1 = createMember("김"); // 회원1
+        Member member2 = createMember("김"); // 회원2. 같은 정보 들어있음
 
-    public Member createMember() {
+        // when
+        memberService.join(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class,()->{
+            memberService.join(member2);
+        });
+
+        // then
+        assertEquals("이미 있는 아이디임", e.getMessage());
+    }
+
+
+    public Member createMember(String username) {
 
         MemberFormDTO memberFormDTO = new MemberFormDTO();
 
-        memberFormDTO.setUsername("문태준");
+        memberFormDTO.setUsername(username);
         memberFormDTO.setPw("1234");
         memberFormDTO.setEmail("asdf@gmail.com");
         memberFormDTO.setPhone("010-1234-5678");
