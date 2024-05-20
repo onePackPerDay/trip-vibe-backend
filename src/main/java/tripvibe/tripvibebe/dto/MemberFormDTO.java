98ef0e6378ac2c8dd03d2cff77e02662f6d7d0e8
문태준 <pjrnoon99@gmail.com>
2024-05-20 16:22:20 +0900

    [feat] 회원가입 / 로그인 DTO

---
+++ src/main/java/tripvibe/tripvibebe/dto/MemberFormDTO.java
@@ -0,0 +1,28 @@
+package tripvibe.tripvibebe.dto;
+// 회원가입 / 로그인 DTO
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class MemberFormDTO {
+
+    // MemberFormDTO : 회원가입 화면에서 넘어온 가입 정보를 담는 객체
+    private String username;
+
+    private String pw;
+
+    private String email;
+
+    private String phone;
+
+    private LocalDate birth;
+
+    private String gender;
+
+    private String mbti;
+
+}
