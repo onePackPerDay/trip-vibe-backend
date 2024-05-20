6f2e14af6aaba446585f5421b11cfc1dd813f272
문태준 <pjrnoon99@gmail.com>
2024-05-20 15:30:50 +0900

    [feat] SecurityConfig 생성

---
+++ src/main/java/tripvibe/tripvibebe/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package tripvibe.tripvibebe.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+////        http.csrf((csrfConfigurer -> csrfConfigurer.disable()));
+//        http.authorizeHttpRequests((authorize) ->
+//                authorize.requestMatchers("/**").permitAll()
+//        );
+//        return http.build();
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
