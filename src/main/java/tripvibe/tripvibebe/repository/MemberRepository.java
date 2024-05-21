67387983f07aa93ae22af2bd9638df28a0a2e0b4
문태준 <pjrnoon99@gmail.com>
2024-05-21 16:43:38 +0900

    [fix] 주석

--- src/main/java/tripvibe/tripvibebe/repository/MemberRepository.java
+++ src/main/java/tripvibe/tripvibebe/repository/MemberRepository.java
@@ -9,7 +9,8 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
-//    Optional<Member> findByUsername(String username);
-
+    //    Optional<Member> findByUsername(String username);
+    // 새거
+//    Member findByEmail(String email); // 이메일로 중복회원 검사
 
 }
