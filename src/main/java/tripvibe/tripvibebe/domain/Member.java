538534ca0ba5032c1289033e6742b0fe4964797b
문태준 <pjrnoon99@gmail.com>
2024-05-21 16:40:53 +0900

    [setting] username -> memberId

--- src/main/java/tripvibe/tripvibebe/domain/Member.java
+++ src/main/java/tripvibe/tripvibebe/domain/Member.java
@@ -2,6 +2,8 @@ package tripvibe.tripvibebe.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tripvibe.tripvibebe.dto.MemberFormDTO;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -17,11 +19,11 @@ public class Member {
 
     @Id //primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) //기본 키 값을 자동으로 생성
-    @Column(name = "member_id")
+    @Column(name = "account_num")
     private  Long id;
 
     @Column(nullable = false, unique = true) // 변경 @NotEmpty
-    private String username;
+    private String memberId;
 
     @Column(nullable = false) // 변경 @NotEmpty
     private String pw;
@@ -39,5 +41,22 @@ public class Member {
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews = new ArrayList<>();
+
+
+    // 새거
+//    public static Member createMember(MemberFormDTO memberFormDTO, PasswordEncoder passwordEncoder) {
+//        Member member = new Member();
+//
+//        member.setMemberId(memberFormDTO.getMemberId());
+//        member.setPw(passwordEncoder.encode(memberFormDTO.getPw()));
+//        member.setEmail(memberFormDTO.getEmail());
+//        member.setPhone(memberFormDTO.getPhone());
+//        member.setBirth(memberFormDTO.getBirth());
+//        member.setGender(memberFormDTO.getGender());
+//        member.setMbti(memberFormDTO.getMbti());
+//
+//        return member;
+//    }
+
 
 }
