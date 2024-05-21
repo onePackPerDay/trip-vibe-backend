f8106dbb04f2a90b3a1a4e23f905b9cca0cdc8f2
문태준 <pjrnoon99@gmail.com>
2024-05-21 20:16:15 +0900

    [fix] 주석 삭제

--- src/main/java/tripvibe/tripvibebe/service/MemberService.java
+++ src/main/java/tripvibe/tripvibebe/service/MemberService.java
@@ -7,10 +7,12 @@ import tripvibe.tripvibebe.domain.Member;
 import tripvibe.tripvibebe.dto.MemberDTO;
 import tripvibe.tripvibebe.repository.MemberRepository;
 
+import java.util.Optional;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService{
 
     private final MemberRepository memberRepository;
 
@@ -63,40 +65,5 @@ public class MemberService {
         memberRepository.save(member);
     }
 
-
-
-    /**
-     * 회원가입
-     */
-//    @Transactional(readOnly = false)
-//    public Long join(Member member) {
-//        validateDuplicateMember(member);
-//        memberRepository.save(member);
-//
-//        return member.getId();
-//    }
-
-    /**
-     * 회원 전체 조회
-     */
-//    public List<Member> findMembers() {
-//        return memberRepository.findAll();
-//    }
-
-
-    // 중복 회원 검증 메서드
-//    private void validateDuplicateMember(Member member) {
-//        Optional<Member> findMember = memberRepository.findByUsername(member.getUsername());// 같은 이름이 있는지 찾아봄
-//        if (findMember != null) {
-//            throw new IllegalStateException("이미 있는 아이디임");
-//        }
-//    }
-
-    // 로그인
-//    public MemberDTO findBy(final MemberRequestDTO params) {
-//        MemberDTO entity = memberRepository.findByUsernameAndPw(params.getUsername(), params.getPw());
-//
-//        return entity;
-//    }
 
 }
