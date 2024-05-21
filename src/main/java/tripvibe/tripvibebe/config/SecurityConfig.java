27def00853f088587c67addac7103c1fb8b5a80f
문태준 <pjrnoon99@gmail.com>
2024-05-21 20:15:18 +0900

    [setting] spring SecurityConfig

--- src/main/java/tripvibe/tripvibebe/config/SecurityConfig.java
+++ src/main/java/tripvibe/tripvibebe/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,11 +17,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/**").permitAll()  // 특정 경로를 허용
-                        .anyRequest().authenticated()               // 나머지 모든 요청에 대해 인증 요구
-                );
+        http.csrf((csrf) -> csrf.disable()); // 나중에 이거 삭제해야함
+        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                        .requestMatchers("/**").permitAll());  // 특정 경로를 허용
+
+        http.formLogin((formLogin)
+                -> formLogin.loginPage("/tripvibe/signin") // 로그인 폼이 있는 페이지 url
+                .defaultSuccessUrl("/") // 로그인 성공시 url
+                .failureUrl("/fail")// 로그인 실패시 url
+        );
         return http.build();
     }
 
