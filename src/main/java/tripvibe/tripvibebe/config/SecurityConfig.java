edaea683f69aac473a9ea8ad33f6746cac60842f
문태준 <pjrnoon99@gmail.com>
2024-05-22 15:39:42 +0900

    [fix] requestMatchers /tripvibe/mypage/** -> /** (전체 허용으로 변경)

--- src/main/java/tripvibe/tripvibebe/config/SecurityConfig.java
+++ src/main/java/tripvibe/tripvibebe/config/SecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf((csrf) -> csrf.disable()); // 나중에 이거 삭제해야함
         http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers("/tripvibe/mypage/**").permitAll());  // 특정 경로를 허용
+                        .requestMatchers("/**").permitAll());  // 특정 경로를 허용
 
         http.formLogin((formLogin)
                 -> formLogin.loginPage("/tripvibe/signin") // 로그인 폼이 있는 페이지 url
