d4f775e7882fdaf9471d71b79a7e92db514df581
문태준 <pjrnoon99@gmail.com>
2024-05-22 23:18:00 +0900

    [fix] jwt 아이디+비번 / 아이디 존재여부 추가

--- src/main/java/tripvibe/tripvibebe/repository/MemberRepository.java
+++ src/main/java/tripvibe/tripvibebe/repository/MemberRepository.java
@@ -10,4 +10,9 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByMemberId(String memberId);
 
+    // DB에서 아이디 + 비번 같이 찾아옴. 로그인 시 id pw 일치여부
+    boolean existsByMemberIdAndPw(String memberId, String pw);
+
+    boolean existsByMemberId(String memberId);
+
 }
