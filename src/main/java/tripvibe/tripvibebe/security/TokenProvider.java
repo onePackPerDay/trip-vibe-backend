9d2cf0eec3b32f5064b1ddcf035605219b8a8686
문태준 <pjrnoon99@gmail.com>
2024-05-23 10:35:10 +0900

    [fix] 토큰 인코딩 추가

--- src/main/java/tripvibe/tripvibebe/security/TokenProvider.java
+++ src/main/java/tripvibe/tripvibebe/security/TokenProvider.java
@@ -5,15 +5,26 @@ import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.time.Instant;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
+@Setter
 public class TokenProvider {
 
-    private static final String SECURITY_KEY = "inputYourSecurityKey"; // 암호화된 JWT
+    private static String SECURITY_KEY; // 암호화된 JWT
+
+    static{
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[64];
+        random.nextBytes(bytes);
+        SECURITY_KEY = Base64.getEncoder().encodeToString(bytes);
+    }
 
     // jwt 생성 메서드 : MemberId와 만료시간을 받아 JWT를 생성하고 서명한 후 직렬화된 JWT문자열을 반환함
     public String createJwt(String memberId, int duration) { // duration : JWT만료시간(초)
@@ -23,14 +34,14 @@ public class TokenProvider {
             Instant now = Instant.now(); // 현재시간
             Instant expirationTime = now.plusSeconds(duration); // 현재시간 + duration(만료시간)
 
-            // JWT 클레임
+            // JWT 클레임 : 내용설정
             JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
                     .subject(memberId) // 'sub' 클레임에 사용자 아이디 설정
                     .issueTime(Date.from(now)) // 'iat' 클레임에 발행시간 설정
                     .expirationTime(Date.from(expirationTime)) // 'exp' 클레임에 만료시간 설정
                     .build();
 
-            // JWT 서명 : HS256 알고리즘을 사용하여 JWT를 서명할 준비를 함
+            // JWT 서명 : HS256 알고리즘을 사용하여 JWT를 서명할 준비를 함, 헤더 설정
             SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
 
 
@@ -61,4 +72,6 @@ public class TokenProvider {
             return null;
         }
     }
+
+
 }
